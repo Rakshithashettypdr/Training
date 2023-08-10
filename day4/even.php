c9a53f76c9f81abccd21609f55638d023f6e22b9
Rakshithashettypdr <129877398+Rakshithashettypdr@users.noreply.github.com>
2023-08-10 15:29:19 +0530

    Add files via upload

---
+++ day4/even.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Even or Odd</title>
+</head>
+<body>
+<?php
+$a=10;
+if($a%2==0)
+{
+echo "given number is"."<br></br>"."<i><b>Even</b></i>";
+}
+else
+{
+echo "given number is"."<br></br>"."<i><b>Odd</b></i>";
+}
+?>
+</body>
+</html>
